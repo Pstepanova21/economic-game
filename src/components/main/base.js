564a97e6ed31ea2873c79d6988a7f00d00507163
Polina <polina_stepanova7741@mail.ru>
2024-08-12 12:05:02 +1100

    fixed

--- src/components/main/base.js
+++ src/components/main/base.js
@@ -1,14 +1,10 @@
 import React from "react";
-
+import logo from "../../assets/image/logo.png";
 const Header = () => {
   return (
     <header className="desktop-header">
       <div className="container-header">
-        <img
-          className="displayed"
-          src="https://psv4.userapi.com/c237231/u367891069/docs/d5/aac3e99c15bf/5345345345345.png?extra=tXc2_RVSoR8LBX_1htZRdFkROdgdo-ZLY2kJ1khJlpbeFutOR1AIxzjUsqHJXhx0RyfW6eso-FbXtjpIOTCXuoUrvlvpZn8-ahrq5dnfreqCLAyMl-coydtX7QH1FWX9gk57zUVIm5gujpRZ4RWSZ5oY"
-          alt="logo"
-        />
+        <img className="displayed" src={logo} alt="logo" />
         <h1 className="main_header">
           Экономическая игра <span className="year_20">20</span>
           <span className="year_24">24</span>
